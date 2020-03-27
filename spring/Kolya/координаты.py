00116dfec59d02fab47af31c5ba509436b2354de
RobolandStudent <59055709+RobolandStudent@users.noreply.github.com>
2020-03-27 11:57:45 +0300

    Add files via upload

---
+++ spring/Kolya/координаты.py
@@ -0,0 +1,29 @@
+import turtle
+t1 = turtle.Pen ()
+t1.penup()
+t1.goto(20,20)
+t1.pendown()
+
+t1.goto(20,170)
+t1.goto(120, 170)
+t1.goto(120,20)
+t1.goto(20,20)
+
+t1.penup()
+t1.goto(-170,120)
+t1.pendown()
+
+t1.goto(-170,20)
+t1.goto(-20,20)
+t1.goto(-20,120)
+t1.goto(-170,120)
+t1.goto(-95,70)
+t1.goto(-20,120)
+
+
+
+
+
+t1.screen.mainloop()
+
+
